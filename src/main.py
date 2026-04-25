e6182797892596e543779cf7a748e1bb9d6f59aa
Trevor Stepp <181792165+trevorstepp@users.noreply.github.com>
2026-04-25 00:18:21 -0400

    almost finish building training loop

--- src/main.py
+++ src/main.py
@@ -4,14 +4,50 @@ from split_and_scale import split_and_scale_data
 from model import HurricaneLSTM
 
 import torch
+import torch.nn as nn
 from torch.utils.data import TensorDataset, DataLoader
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 DATA_DIR = BASE_DIR / "data"
 
-def training_loop() -> None:
-    pass
+def training_loop(model: nn.Module, train_loader: DataLoader, test_loader: DataLoader,
+                  epochs: int = 10, lr: float = 0.001) -> tuple[list[float], list[float]]:
+    """
+    Train the HurricaneLSTM model.
+    """
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters, lr)
+    best_loss = 0.0
+
+    for epoch in range(epochs):
+        train_loss = 0.0
+
+        for X_batch, y_batch in train_loader:
+            # reset gradients
+            optimizer.zero_grad()
+
+            outputs = model(X_batch)
+            loss = criterion(outputs, y_batch)
+            loss.backward()
+            optimizer.step()
+
+            train_loss += loss.item()
+        
+        train_loss /= len(train_loader)
+
+        # evaluate model each epoch, stop when loss stops improving
+        model.eval()
+        test_loss = 0.0
+
+        with torch.no_grad():
+            for X_test, y_test in test_loader:
+                preds = model(X_test)
+                loss = criterion(preds, y_test)
+                test_loader += loss.item()
+        
+        test_loss /= len(test_loader)
+        print(f"Epoch {epoch + 1}: Train = {train_loss:.4f}, Test = {test_loss:.4f}")
 
 def main() -> None:
     """
@@ -58,6 +94,8 @@ def main() -> None:
         layer_dim=2,
         output_dim=y_train.shape[-1]
     )
+
+    training_loop(model, train_loader, test_loader)
 
 if __name__ == "__main__":
     main()
