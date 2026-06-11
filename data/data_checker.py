a7e02341772615d640e03cd34eee0186162fc039
Trevor Stepp <181792165+trevorstepp@users.noreply.github.com>
2026-06-11 13:04:06 -0500

    structure changes

--- data/data_checker.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-if __name__ == '__main__':
-    df = pd.read_csv("parsed_hurdat2.csv")
-    print(df["storm_id"].str[:2].value_counts())
---
+++ src/data_processing/era5/constants.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parents[3]
+ERA5_DIR = BASE_DIR / "data" / "era5"
